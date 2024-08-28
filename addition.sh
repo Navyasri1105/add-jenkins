89ab567132accf952679d0890e52fc048751494a
navya <jenkins@123gmail.com>
2024-08-28 16:09:49 +0530

    hi

---
+++ addition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:5 "
+
+
+# Ask user for second number
+echo "Enter second number:6 "
+
+
+# Perform addition
+sum=$((5 + 6))
+
+# Display result
+echo "Sum: $sum"
